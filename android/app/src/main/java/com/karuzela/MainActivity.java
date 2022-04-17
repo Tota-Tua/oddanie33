f116014a1b1917b77b75486fab69378d439227dd
JosephPrzemyslaw <josephmac@interia.pl>
2022-04-17 11:40:47 +0200

    Changing project name from karuzela to oddanie33 using react-native-rename

--- android/app/src/main/java/com/karuzela/MainActivity.java
+++ android/app/src/main/java/com/karuzela/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "karuzela";
+    return "oddanie33";
   }
 
   /**
--- ios/oddanie33Tests/oddanie33Tests.m
+++ ios/oddanie33Tests/oddanie33Tests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface karuzelaTests : XCTestCase
+@interface oddanie33Tests : XCTestCase
 
 @end
 
-@implementation karuzelaTests
+@implementation oddanie33Tests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
