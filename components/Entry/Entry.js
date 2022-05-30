5c1436b4be18571b16ab38f0a8b87bb06b772b14
JosephPrzemyslaw <josephmac@interia.pl>
2022-05-30 11:34:19 +0200

    Adding linear gradient dependent on theme value

--- components/Entry/Entry.js
+++ components/Entry/Entry.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import {SafeAreaView, View} from 'react-native';
-// import LinearGradient from 'react-native-linear-gradient';
+import {useSelector} from 'react-redux';
+import {/*ImageBackground,*/ SafeAreaView, View} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 import {Icon, TopNavigation, TopNavigationAction} from '@ui-kitten/components';
 import MainCarousel from '../MainCarousel/MainCarousel';
-import styles /*, {colors}*/ from '../Entry/Entry.styles';
+import styles, {colors} from '../Entry/Entry.styles';
+//import images from '../../res/images/images';
+import store from '../../store/store';
 
 const MenuIcon = props => <Icon {...props} name="menu-outline" />;
 
@@ -20,17 +23,31 @@ const MenuAction = (navigation, props) => (
    I did not notice for SafeAreaView changed the way how app is rendered on my phone (Oppo Reno 3 Pro), anyway I leave it just in case
 */
 const Entry = ({navigation}) => {
+  const mode = store.getState().settings.darkMode;
+  useSelector(state => state.settings.darkMode);
+
+  const theme = mode ? colors.dark : colors.light;
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
-        <TopNavigation accessoryLeft={MenuAction.bind(undefined, navigation)} />
-        {/* <LinearGradient
-          colors={[colors.background1, colors.background2]}
+        <LinearGradient
+          colors={[theme.background1, theme.background2]}
           startPoint={{x: 1, y: 0}}
           endPoint={{x: 0, y: 1}}
-          style={styles.gradient}
-        /> */}
-        <MainCarousel navigation={navigation} />
+          style={styles.gradient}>
+          <TopNavigation
+            accessoryLeft={MenuAction.bind(undefined, navigation)}
+          />
+          <MainCarousel navigation={navigation} />
+          {/* <View style={styles.logo}>
+            <ImageBackground
+              source={images.oddanie33}
+              resizeMode="contain"
+              style={{flex: 1,justifyContent: 'center'}}
+            >
+            </ImageBackground>
+          </View> */}
+        </LinearGradient>
       </View>
     </SafeAreaView>
   );
--- components/Entry/Entry.styles.js
+++ components/Entry/Entry.styles.js
@@ -6,16 +6,14 @@ export default StyleSheet.create({
   safeArea: {
     flex: 1, // flexGrow: 1 - automatically resizes the compoenent to fit the whole screen
     flexDirection: 'column',
-    //margin: 10,
-    backgroundColor: 'red',
   },
   container: {
     flex: 1, // makes the view to be resized to the parent (in this case to the bottom edge)
     //height: screenHeight - statusBarHeight - 300, // 20 is experimental, probably the height of scrren name bar
-    borderColor: 'blue',
-    borderRadius: 1,
-    borderWidth: 1,
-    backgroundColor: '#6ab3eb', //colors.background1
+  },
+  logo: {
+    //flex: 1/10,
+    padding: 5,
   },
   gradient: {
     ...StyleSheet.absoluteFillObject,
@@ -23,6 +21,12 @@ export default StyleSheet.create({
 });
 
 export const colors = {
-  background1: '#6ab3eb',
-  background2: '#d2d7e7',
+  light: {
+    background1: '#6ab3eb',
+    background2: '#d2d7e7',
+  },
+  dark: {
+    background1: '#091C7A',
+    background2: '#1A34B8',
+  },
 };
