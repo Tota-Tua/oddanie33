aff2e756f84918f4a5523340da060fde45c232ee
JosephPrzemyslaw <josephmac@interia.pl>
2022-06-16 11:19:45 +0200

    :feature: Handling lack of internet in DayDetails screen

--- components/DayDetails/DayDetails.js
+++ components/DayDetails/DayDetails.js
@@ -1,14 +1,6 @@
 import React, {useEffect, useRef, useState} from 'react';
 import WebView from 'react-native-webview';
-import {
-  CheckBox,
-  Divider,
-  Icon,
-  Layout,
-  Text,
-  TopNavigation,
-  TopNavigationAction,
-} from '@ui-kitten/components';
+import {CheckBox, Divider, Layout, Text} from '@ui-kitten/components';
 import {useDispatch} from 'react-redux';
 import {TouchableOpacity} from 'react-native';
 import Orientation from 'react-native-orientation-locker';
@@ -18,12 +10,9 @@ import {
   add as addCompleted,
   remove as removeCompleted,
 } from '../../store/reducers/completed';
-import {
-  remove as removeFavorite,
-  save as saveFavorite,
-  updateRetreatList,
-} from '../../store/reducers/favorites';
 import {styles} from './DayDetails.styles';
+import ThisTopNavigation from './TopNavigation';
+import ErrorPage from './ErrorPage';
 
 function isCompleted(url) {
   const completed = store.getState().completed;
@@ -32,11 +21,12 @@ function isCompleted(url) {
 
 const DELAY_BEFORE_USING_WEBVIEW = 1000;
 const DEFAULT_TOP_NAVIGATION_HEIGHT = 56;
-const DayDetails = ({navigation, route: {params}}) => {
-  // the intention is to prevent from navigation
-  const injectedJSCode =
-    '["nav", "footer", ".nav"].forEach(el => document.querySelector(el).remove());';
+// to prevent from unwanted navigation
+const injectedJSCode =
+  '["nav", "footer", ".nav"].forEach(el => document.querySelector(el).remove());';
+import NetworkInfoObj from '../../services/NetworkInfo';
 
+const DayDetails = ({navigation, route: {params}}) => {
   const [isFetching, setFetching] = useState(false);
   const [isDone, setIsDone] = useState(isCompleted(params.item.url));
   const [topNavigationOffset, setTopNavigationOffset] = useState(
@@ -53,8 +43,6 @@ const DayDetails = ({navigation, route: {params}}) => {
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isDone]);
-
-  useEffect(() => setFetching(true), [params.item.url]);
 
   useEffect(() => {
     Orientation.unlockAllOrientations();
@@ -88,85 +76,27 @@ const DayDetails = ({navigation, route: {params}}) => {
       </TouchableOpacity>
       <Divider />
 
-      <WebView
-        style={styles.webView}
-        originWhitelist={[]}
-        source={{uri: params.item.url}}
-        injectedJavaScript={injectedJSCode}
-        onLoadEnd={() =>
-          setTimeout(() => {
-            isMountedRef.current && setFetching(false);
-          }, DELAY_BEFORE_USING_WEBVIEW)
-        }
-        allowsFullscreenVideo={true} // does not work yet correctly with rotation */
-      />
+      {NetworkInfoObj.isConnected ? (
+        <WebView
+          style={styles.webView}
+          originWhitelist={[]}
+          source={{
+            uri: params.item.url,
+          }}
+          injectedJavaScript={injectedJSCode}
+          onLoadStart={() => setFetching(true)}
+          onLoadEnd={() =>
+            setTimeout(() => {
+              isMountedRef.current && setFetching(false);
+            }, DELAY_BEFORE_USING_WEBVIEW)
+          }
+          allowsFullscreenVideo={true} // does not work yet correctly with rotation */
+        />
+      ) : (
+        <ErrorPage navigation={navigation} />
+      )}
       <Spinner visability={isFetching} style={{top: topNavigationOffset}} />
     </Layout>
-  );
-};
-
-const ThisTopNavigation = props => {
-  const item = props.item;
-  function isFavorite(currItem) {
-    const favorites = store.getState().favorites;
-    return favorites.items.some(
-      refItem => JSON.stringify(refItem) === JSON.stringify(currItem),
-    );
-  }
-
-  const BackIcon = myProps => <Icon {...myProps} name="arrow-back" />;
-  const BackAction = () => (
-    <TopNavigationAction
-      icon={BackIcon}
-      onPressIn={() => props.navigation.goBack()}
-    />
-  );
-
-  const dispatch = useDispatch();
-  const [selected, setSelected] = useState(isFavorite(item));
-  const [isDispatchBlocked, setDispatchBlocked] = useState(true);
-  const SettingsAction = () => {
-    const HeartIcon = heartProps => {
-      useEffect(() => {
-        if (!isDispatchBlocked) {
-          const action = selected ? saveFavorite : removeFavorite;
-          dispatch(action(item));
-          dispatch(updateRetreatList());
-          setDispatchBlocked(true);
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-      }, [selected]);
-
-      return (
-        <Icon
-          {...heartProps}
-          name="heart"
-          {...(selected ? {fill: '#FF0000'} : {})}
-        />
-      );
-    };
-
-    return (
-      <TopNavigationAction
-        icon={HeartIcon}
-        onPressIn={() => {
-          setSelected(oldVal => {
-            setDispatchBlocked(false);
-            return !oldVal;
-          });
-        }}
-      />
-    );
-  };
-
-  return (
-    <TopNavigation
-      alignment="center"
-      accessoryLeft={BackAction}
-      accessoryRight={SettingsAction}
-      title="Opis rekolekcji"
-      onLayout={props.onLayout}
-    />
   );
 };
 
